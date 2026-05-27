b9193291da1a2358ddbc01bae3f81e206e7bce00
Darlyng Mateo <159739591+darlyng405@users.noreply.github.com>
2026-05-27 19:46:29 -0400

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = (() => {
-  if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    return 'http://localhost:3000/api';
-  }
-  return 'https://votatech-backend.onrender.com/api';
-})();
-
-export { API_BASE_URL };
