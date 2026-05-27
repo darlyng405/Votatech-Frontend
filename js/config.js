f5471a19119ababc69ff314d00c42c2e7a658af1
Darlyng Mateo <159739591+darlyng405@users.noreply.github.com>
2026-05-27 19:18:13 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const API_BASE_URL = (() => {
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     return 'http://localhost:3000/api';
   }
-  return 'https://votatech-backend.onrender.com/api';
+  return 'https://votatech-frontend.vercel.app/';
 })();
 
 export { API_BASE_URL };
